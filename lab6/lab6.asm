d31352cb81d7de0a302f3ae20dbb0bcc3ccde3c9
Horațiu Udrea <udrea.horatiu@gmail.com>
2018-12-02 00:33:54 +0200

    Corrected lab6

--- lab6/lab6.asm
+++ lab6/lab6.asm
@@ -9,8 +9,6 @@ import exit msvcrt.dll
 segment data use32 class=data
     sir DD 12AB5678h, 1256ABCDh, 12344344h
     len EQU ($-sir)/4
-    dest RESD len
-    destLen DD 0
 
 segment code use32 class=code
     start:
@@ -18,39 +16,28 @@ segment code use32 class=code
         MOV ESI, sir
         CLD
         JECXZ finish
-        sir_elements_loop:
-            MOV EAX, 0
-            LODSW ; take the low word
-            LODSW ; take the high word in AX
-            
-            push ECX ; save the counter value to the stack
-            MOV ECX, [destLen] ; we compare with all the elements in reverse order
-            JECXZ skip
-            dest_elements_loop: ; insertion sort
-                ; ECX - 1 is the index of the element to be compared with AX
-                ; note that multiplication by 4 is required for pointer arithmetic (dest contains doublewords)
-                CMP [dest + (ECX - 1) * 4], EAX ; compare the words (high part of the doublewords is 0)
-                JB skip ; if it the element is bigger, we don't have to compare anymore
-                MOV EBX, [dest + (ECX - 1) * 4]
-                MOV [dest + ECX * 4], EBX ; move the element to the right
-            LOOP dest_elements_loop
-            skip:
-            MOV [dest + ECX * 4], EAX; put the doubleword where it belongs (ECX is the index)
-            INC dword [destLen] ; an element was added
-            pop ECX ; get the counter value back from the stack
-        LOOP sir_elements_loop
-        
-        MOV ECX, len
-        MOV ESI, sir
-        MOV EDI, 0 ; index for dest string
-        sir_elements_loop_2:
-            LODSW ; take the low word in AX
-            SHL dword [dest + EDI], 8 * 2
-            MOV word [dest + EDI], AX ; insert AX as the low part
-            LODSW ; take the high word of the sir string
-            ADD EDI, 4 ; increment the index with a doubleword
-        LOOP sir_elements_loop_2
-        
+        loop_1:
+            PUSH dword ECX
+            DEC ECX
+            JECXZ end_loop
+            LODSW
+            LODSW
+            MOV EDX, ESI
+            MOV BX, AX
+            loop_2:
+                LODSW
+                LODSW
+                CMP BX, AX
+                JNA skip
+                MOV [ESI - 2], BX
+                MOV [EDX - 2], AX
+                MOV BX, AX
+                skip:
+            LOOP loop_2
+            end_loop:
+            MOV ESI, EDX
+            POP ECX
+        LOOP loop_1
         finish:
-        push    dword 0
-        call    [exit]
+        PUSH dword 0
+        CALL [exit]
